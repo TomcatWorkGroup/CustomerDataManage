2ba64a16a06fa249f9428ae7b54ca549ecd415e4
80201288 <80201288@qq.com>
2018-03-05 00:11:07 +0800

    完成客户API 数据访问功能

--- src/main/java/com/itdreamworks/customerdatamanage/handler/GlobalExceptionHandler.java
+++ src/main/java/com/itdreamworks/customerdatamanage/handler/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.itdreamworks.customerdatamanage.handler;
 
 import com.itdreamworks.customerdatamanage.entity.enums.ResultStatus;
 import com.itdreamworks.customerdatamanage.utils.ResultStatusJsonStringUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Component;
@@ -15,20 +16,23 @@ import java.io.IOException;
 @Component
 @ControllerAdvice
 @ResponseBody
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public String handleException(Exception e) throws IOException {
+        log.error(e.getMessage());
         return ResultStatusJsonStringUtil.getStatusString(ResultStatus.ERROR,"程序内部错误！");
     }
-
     @ExceptionHandler(DataIntegrityViolationException.class)
     public String handleException(DataIntegrityViolationException e) throws IOException {
+        log.error(e.getMessage());
         return ResultStatusJsonStringUtil.getStatusString(ResultStatus.ERROR,
                 e.getCause().getMessage());
     }
     @ExceptionHandler(DuplicateKeyException.class)
     public String handleException(DuplicateKeyException e) throws IOException {
+        log.error(e.getMessage());
         return ResultStatusJsonStringUtil.getStatusString(ResultStatus.ERROR,e.getCause().getMessage());
     }
 
